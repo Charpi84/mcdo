73c2e08490329a2f1f72117706955d4b43ae55f1
Christelle-Charp <christelle.charpinet@outlook.com>
2025-03-20 16:08:31 +0100

    ajout html categorie et script fonction openCategorie

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let donnes = [];
+let detailCategorieGrid = document.getElementById('detailCategorieGrid');
 
 fetch('mcdo.json')
     .then(function(reponse){
@@ -12,8 +13,54 @@ fetch('mcdo.json')
     .then(function(data){
         donnes = data;
         console.log(donnes);
+        openCategorie();
     })
 
     .catch(function(error) {
         console.error("Erreur lors du chargement du fichier JSON: ", error);
     });
+
+   
+
+    function openCategorie(categorie) {
+                
+        let produits = donnes[categorie];
+
+        detailCategorieGrid.innerHTML = "";
+
+        for (let i = 0; i < produits.length; i++) {
+            let produit = produits[i];
+
+            let card = document.createElement("div");
+            card.classList.add("card");
+            let contenuCard = `<button class="detail-btn" onclick="ajouterPanier()">
+							        <img src="${produit.image}" alt="${produit.name}">
+							        <h3>${produit.name}</h3>
+							        <p class="prix">${produit.price}€</p>
+						        </button>
+						        <button class="info-btn" onclick="afficherInformation()">
+                                    i
+                                </button>`
+            card.innerHTML = contenuCard;
+            detailCategorieGrid.appendChild(card);
+        }
+    }
+
+     /*function openCategorie(burgers) {
+        //let burgers = data.burgers
+
+        for (let i = 0; i < burgers.length; i++) {
+            let card = document.createElement("div");
+            card.classList.add("card");
+            let contenuCard = `<button class="detail-btn" onclick="ajouterPanier()">
+							        <img src="${burgers[i].image}" alt="${burgers[i].name}">
+							        <h3>${burgers[i].name}</h3>
+							        <p class="prix">${burgers[i].price}€</p>
+						        </button>
+						        <button class="info-btn" onclick="afficherInformation()">
+                                    i
+                                </button>`
+            card.innerHTML = contenuCard;
+            detailCategorieGrid.appendChild(card);
+        }
+    }*/
