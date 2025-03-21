cb08e136f4deb0aa7b3bdb16dd3349ea66330e57
Christelle-Charp <christelle.charpinet@outlook.com>
2025-03-21 15:23:26 +0100

    modif script

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ fetch('mcdo.json')
             let produit = produits[i];
 
             let card = document.createElement("div");
-            let contenuCard = `<div classe = "card" onclick="ajouterPanier(${produit.id})">
+            let contenuCard = `<div class = "card" onclick="ajouterPanier(${produit.id})">
 							        <img src="${produit.image}" alt="${produit.name}">
 							        <h3>${produit.name}</h3>
 							        <p class="prix">${produit.price}€</p>
@@ -74,7 +74,7 @@ fetch('mcdo.json')
     
                     let cardProduit = document.createElement("div");
                     cardProduit.classList.add("produit");
-                    let contenuCardProduit = `<div class="card">
+                    let contenuCardProduit = `<div class="modal-product">
                                                 <button class="close-btn" onclick="close()">X</button>
                                                 <img src="${produit.image}" alt="${produit.name}">
                                                 <div class="encart-card">
